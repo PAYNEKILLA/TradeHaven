9a46cc285b5dffc58f5191512412accc8d9c3d5d
PAYNEKILLA <ISAHUSMAN4961@GMAIL.COM>
2025-09-04 07:55:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ import os
 
 app = Flask(__name__)
 
-@app.route()
-def home()
-    return 🚀 Trade Haven is live!
+@app.route("/")
+def home():
+    return "Trade Haven is live"
 
-if __name__ == __main__
-    port = int(os.environ.get(PORT, 8080))
-    app.run(host=0.0.0.0, port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port) 
